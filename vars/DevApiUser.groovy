0bef0b72dd78adb6ea60124fe0df9b14b3720ff1
Takashi Kakinari <kakinari@me.com>
2019-12-12 20:18:53 +0900

    typo

--- vars/DevApiUser.groovy
+++ vars/DevApiUser.groovy
@@ -4,5 +4,5 @@ def call() {
     return new RemoteUser(
         credential:   'f39cac72-640d-451c-a943-fc4884203cee',
         username:     'root',
-        hostname:     'dev.api.intra.telsys.co.jp'))
+        hostname:     'dev.api.intra.telsys.co.jp')
 }
