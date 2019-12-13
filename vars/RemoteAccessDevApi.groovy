c53e591decfc0a44ab893d27917ba0f54bb80673
Takashi Kakinari <kakinari@me.com>
2019-12-13 14:22:53 +0900

    fix2

--- vars/RemoteAccessDevApi.groovy
+++ vars/RemoteAccessDevApi.groovy
@@ -5,5 +5,5 @@ def call() {
     return new RemoteAccess(new RemoteUser (
         credential:   'f39cac72-640d-451c-a943-fc4884203cee',
         username:     'root',
-        hostname:     'dev.api.intra.telsys.co.jp')
+        hostname:     'dev.api.intra.telsys.co.jp'))
 }
