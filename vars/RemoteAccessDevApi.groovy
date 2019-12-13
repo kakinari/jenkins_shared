5d1ebaf3144425848559ea861a397e8adda225c6
Takashi Kakinari <kakinari@me.com>
2019-12-13 17:19:36 +0900

    move to private

--- vars/RemoteAccessDevApi.groovy
+++
@@ -1,9 +0,0 @@
-import com.kakinari.jenkins.RemoteUser
-import com.kakinari.jenkins.RemoteAccess
-
-def call(steps) {
-    return new RemoteAccess(steps, new RemoteUser (
-        credential:   'f39cac72-640d-451c-a943-fc4884203cee',
-        username:     'root',
-        hostname:     'dev.api.intra.telsys.co.jp'))
-}
