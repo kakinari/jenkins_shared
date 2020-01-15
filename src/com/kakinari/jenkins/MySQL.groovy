5316b2cbc61cdf856b896ede1ef7b4e966656773
Takashi Kakinari <kakinari@me.com>
2020-01-15 19:40:12 +0900

    define getQuery

--- src/com/kakinari/jenkins/MySQL.groovy
+++ src/com/kakinari/jenkins/MySQL.groovy
@@ -16,7 +16,7 @@ class MySQL implements Serializable {
         this.username = info?.get('username') ?: null
         this.password = info?.get('password') ?: null
         this.schema    = info?.get('schema') ?: null
-        this.template    = info?.get('template') ?: "com/kakinari/jenkins/queries"
+        this.template  = info?.get('template') ?: "com/kakinari/jenkins/queries"
     }
 
     def commandLine(cmd = "mysql") {
@@ -64,6 +64,100 @@ class MySQL implements Serializable {
             steps.sh(script: "zcat ${filename} | ${commandLine()}")
         else
             steps.sh(script: "cat ${filename} |  ${commandLine()}")
+    }
+
+	def getquery(String filename, String colname = null, String data = null, String extra = null, String ordercond = null, String groupcond = null) {
+        if (filename == null)
+            return ""
+        return "${getTemplate(filename)}${getCondition(colname, data, extra, ordercond, groupcond)}"
+    }
+
+	def getCondition(String colname, String data, String extra = null, String ordercond = null, String groupcond = null) {
+        if (colname == null || data == null)
+            return ""
+		String conn = " WHERE ";
+		StringBuffer buff = new StringBuffer();
+		if (extra != null) {
+			buff.append("${conn} ( ${extra} )");
+			conn =  "\n AND (";
+		}
+		buff.append(getConditionString(colname, data, conn));
+		if (extra != null)
+			buff.append(")");
+		if (groupcond != null)
+			buff.append("\n GROUP BY ").append(groupcond);
+		if (ordercond != null)
+			buff.append("\n ORDER BY ").append(ordercond);
+		buff.append(";");
+		return buff.toString();
+	}
+
+      def getConditionString(String colname, String data, String conn = "") {
+        if (colname == null || data == null)
+            return "";
+        StringBuffer buff = new StringBuffer();
+        if (!colname.contains("`") && !colname.contains("("))
+            colname = "`${colname}`";
+        for (String row :data.split("\n")) {
+            if (row == null || row.length() == 0)
+                continue;
+            if (row.contains("&")) {
+                buff.append(conn);
+                String conn2 = " ((";
+                for (String part : row.split("&")) {
+                    buff.append("${conn2} ${colname} ${getConditionString(part)}");
+                    conn2 = ") \n  AND (";
+                }
+                buff.append("))");
+            } else {
+                buff.append("${conn} ${colname} ${getConditionString(row)}");
+            }
+            conn = "\n OR ";
+        }
+        return buff;
+    }
+
+	def  getConditionString(String row) {
+        if (row.contains("-")) {
+            String[] keyval = row.split("-",2);
+            return " BETWEEN \'${keyval[0]}\' AND \'${keyval[1]}\'"
+        } else if (row.contains(",")) {
+            StringBuffer buff = new StringBuffer();
+            buff.append(" IN (");
+            String head = ""
+            row.split(",").each {
+                buff.append("${head} '${it}'")
+                head = ", "
+            }
+            buff.append(")")
+            return buff.toString()
+        } else if (row.startsWith("~")) {
+            return " REGEXP \'${row.substring(1)}\'"
+        } else if (row.startsWith("!~")) {
+            return " NOT REGEXP \'${row.substring(2)}\'"
+        } else if (row.startsWith("=")) {
+            return " = \'${row.substring(1)}\'"
+        } else if (row.startsWith("<>")) {
+            return " <> \'${row.substring(2)}\'"
+        } else if (row.startsWith("<")) {
+            return " < \'${row.substring(1)}\'"
+        } else if (row.startsWith(">")) {
+            return " > \'${row.substring(1)}\'"
+        } else if (row.equals("NULL")) {
+            return " IS NULL"
+        } else if (row.equals("!NULL")) {
+            return " IS NOT NULL"
+        } else if (row.contains("%")) {
+            if (row.contains("_")) {
+                return " LIKE \'${row.replace('_', '\$_')}\' ESCAPE '\$'"
+            } else 
+                return " LIKE \'${row}\'"
+        } else {
+            if (row.contains("_")) {
+                return " LIKE \'${row.replace('_', '\$_')}%\' ESCAPE '\$'"
+            } else 
+                return " LIKE \'${row}%\'"
+        }
     }
 
 }
